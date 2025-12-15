8fd0961dda82bfb208ff0f7ea8f4014d9c957ac8
Victor de Souza Magalhaes <vdesouza@nrao.edu>
2025-12-15 15:10:50 -0700

    improvements on calibration script.

--- etc/beamcuts/beamcut_CASA_calibration.py
+++ etc/beamcuts/beamcut_CASA_calibration.py
@@ -1,27 +1,12 @@
 import os
+import shutil
+
 import numpy as np
 import casatools
-from datetime import datetime, timedelta
 from pathlib import Path
 
 lnbr = '\n'
 spc=' '
-
-def search(string):
-    first = string.find('.')
-    if first == -1:
-        return -1, -1
-    second = string.find('.',first+1)
-    return first+1, second+7
-
-
-def julian_day_to_date(jd):
-    # JD 2440587.5 corresponds to 1970-01-01 00:00:00 UTC
-    unix_epoch = datetime(1970, 1, 1)
-    days_since_unix_epoch = jd - 2440587 #.5
-    dt = unix_epoch + timedelta(days=days_since_unix_epoch)
-    #return dt.strftime("%Y-%m-%d-Hh-%Mm")
-    return dt.strftime("%m-%d-%Y at %Hh%Mm")
 
 def yesno(prompt):
     user_ans = input(f'{prompt} <(Y)es/(N)o>: ').lower()
@@ -34,66 +19,61 @@ def yesno(prompt):
         return yesno(prompt)
 
 
-def is_asdm(filename):
-    file_path = Path(f"{filename}/ASDM.xml")
-    return file_path.exists()
+class MessageBoard:
 
+    def __init__(self, width=60, block_char='#', spacing=1, blocking=3):
+        self.width = width
+        self.block_char = block_char
+        self.spacing = spacing,
+        self.blocking = blocking
 
-def asdm_to_ms(asdm_name, overwrite):
-    msname = asdm_name + '.ms'
-    if os.path.exists(msname) and not overwrite:
-        print(msname+' already exists.')
-    else:
-        importasdm(asdm=asdm_name,
-                   vis=msname,
-                   createmms=False,
-                   ocorr_mode='co',
-                   lazy=False,
-                   asis='Receiver CalAtmosphere',
-                   process_caldevice=True,
-                   process_pointing=True,
-                   savecmds=True,
-                   outfile=msname + '.flagonline.txt',
-                   overwrite=False,
-                   bdfflags=False,
-                   with_pointing_correction=True,
-                   applyflags =True)
-    return msname
+        self.capo = blocking*block_char + spacing*spc
+        self.coda = self.capo[::-1]+lnbr
+        self.usable_width = width - 2*spacing - 2*blocking
+        self.block_line = self.width*self.block_char+lnbr
+        self.block_len = len(self.capo)
 
-
-def create_heading(heading, width=60, block_char='#', spacing=1, blocking=3):
-    outstr = width*block_char+lnbr
-    capo = blocking*block_char + spacing*spc
-    coda = capo[::-1]+lnbr
-    usable_width = width - 2*spacing - 2*blocking
-    heading = heading.strip()
-    head_wrds = heading.split()
-    capo_len = len(capo)
-
-    def end_line(line, usable_width, coda):
-        line_len = len(line) + 1 - capo_len
-        spc_to_add = usable_width - line_len
-        return spc_to_add*spc + coda
-
-    line = capo
-    for wrd in head_wrds:
-        wrd_len = len(wrd)
-        if wrd_len > usable_width:
-            raise ValueError(f'Word {wrd} is larger than the usable width')
-        line_len = len(line) + wrd_len + 1 - capo_len
-        if line_len > usable_width:
-            outstr += line + end_line(line, usable_width, coda)
-            line = capo
+    def end_line(self, line, centered=True):
+        line_len = len(line) + 1 - self.block_len
+        spc_to_add = self.usable_width - line_len
+        if centered:
+            out_line = self.capo + line + self.coda
         else:
-            line += spc + wrd
-    outstr += line + end_line(line, usable_width, coda)
-    outstr += width*block_char+lnbr
-    return outstr
+            out_line = self.capo + line + spc_to_add + self.coda
+        return out_line
+
+    def heading(self, user_msg):
+        outstr = ''
+        outstr += self.block_line
+        head_wrds = user_msg.split()
+
+        line = ''
+        for wrd in head_wrds:
+            wrd_len = len(wrd)
+            if wrd_len > self.usable_width:
+                raise ValueError(f'Word {wrd} is larger than the usable self.width')
+            line_len = len(line) + wrd_len + 1
+            if line_len > self.usable_width:
+                outstr += self.end_line(line)
+                line = wrd
+            else:
+                line += spc + wrd
+        outstr += self.end_line(line)
+        outstr += self.block_line
+        return outstr
+
+    def one_liner(self, msg):
+        if len(msg) > self.usable_width:
+            raise ValueError('Message is larger than usable width')
+        return self.end_line(msg)
+
+    def done(self):
+        return self.one_liner('Done!')
 
 
 class UserInteraction:
     last_use_file = '.beamcut_cal.last'
-    user_inp_list = ['filename', 'field', 'refant', 'overwrite']
+    user_inp_list = ['filename', 'field', 'refant', 'overwrite', 'confirmation_before_start']
     sep = '='
 
     def __init__(self):
@@ -102,6 +82,7 @@ class UserInteraction:
         self.refant = None
         self.overwrite = None
         self.last_use_list = None
+        self.confirmation_before_start = None
 
     def _find_previous_input(self):
         return Path(self.last_use_file).exists()
@@ -116,13 +97,16 @@ class UserInteraction:
         print('Previous inputs:')
         for line in self.last_use_list:
             print(f'\t{line}')
-        return yesno('Re-use previous input?')
+        ans = yesno('Re-use previous input?')
+        print()
+        return ans
 
     def _init_from_user(self):
         self.filename = input("Enter file name: ")
         self.field = input("Enter field number: ")
         self.refant = input("Enter referece antenna: ")
         self.overwrite = yesno('Re-do calibration if already done?')
+        self.confirmation_before_start = yesno('Confirm info before starting calibration?')
 
     def save_input(self):
         outstr = ''
@@ -137,50 +121,88 @@ class UserInteraction:
             if self._reuse_last():
                 for line in self.last_use_list:
                     wrds = line.split(self.sep)
-                    self.__setattr__(wrds[0], wrds[1])
+                    key = wrds[0].strip()
+                    value = wrds[1].strip()
+                    setattr(self, key, value)
             else:
                 self._init_from_user()
         else:
             self._init_from_user()
-        print(self.filename)
 
     @classmethod
     def perform_beamcut_calibration(cls):
-
-        print(create_heading('Welcome to the beam cut calibration pipeline'))
+        msger = MessageBoard()
+        print(msger.heading('Welcome to the beam cut calibration pipeline'))
         my_obj = cls()
         my_obj.read_input()
+        print()
 
-        print(my_obj.filename)
-        if is_asdm(my_obj.filename):
-            msname = asdm_to_ms(my_obj.filename, my_obj.overwrite)
+        mycal_obj = CalObject(my_obj.filename, my_obj.field, my_obj.refant, my_obj.overwrite, msger)
+        if my_obj.confirmation_before_start:
+            proceed = yesno('Proceed with calibration?')
         else:
-            msname = my_obj.filename
+            proceed = True
+        print()
 
-        print(msname)
-        # my_obj.save_input()
-        # exit()
-        # mycal_obj = CalObject(msname, my_obj.field, my_obj.refant, my_obj.overwrite)
-        # mycal_obj.calibration_pipeline()
-        # mycal_obj.apply_calibration()
+        if proceed:
+            my_obj.save_input()
+            mycal_obj.calibration_pipeline()
+            mycal_obj.apply_calibration()
+
+        print(msger.heading('All Done!'))
 
 
 class CalObject:
 
-    def __init__(self, msname, field, refant, overwrite, calversion='01'):
+    def __init__(self, msname, field, refant, overwrite, msger, first_chan=4, last_chan=60):
         self.msname = msname
         self.refant = refant
-        self.overwrite = overwrite
+        self.overwrite = bool(overwrite)
         self.field = field
-        # Supposition this will be
+        self.msger = msger
+        self.fchan = first_chan
+        self.lchan = last_chan
 
-        base_cal_name = msname+'.'+calversion+'.'
+        base_cal_name = msname+'.'
         self.delay_caltable = base_cal_name+'delay.cal'
-        self.bandpass_caltable = 'bandpass.bcal'
+        self.bandpass_caltable = base_cal_name+'bandpass.bcal'
         self.gain_caltable = base_cal_name+'gain.cal'
+
+        if self._is_asdm():
+            print(self.msger.one_liner('Input is an SDM running importasdm...'))
+            self.asdm_to_ms()
+            print(self.msger.done())
 
         self._initialize_metadata()
         self._report_init()
+
+    def _is_asdm(self):
+        file_path = Path(f"{self.msname}/ASDM.xml")
+        return file_path.exists()
+
+    def asdm_to_ms(self):
+        msname = self.msname + '.ms'
+        if os.path.exists(msname) and self.overwrite:
+            print(self.msger.heading('Removing old file'))
+            shutil.rmtree(msname)
+
+        importasdm(asdm=self.msname,
+                   vis=msname,
+                   createmms=False,
+                   ocorr_mode='co',
+                   lazy=False,
+                   asis='Receiver CalAtmosphere',
+                   process_caldevice=True,
+                   process_pointing=True,
+                   savecmds=True,
+                   outfile=msname + '.flagonline.txt',
+                   bdfflags=False,
+                   with_pointing_correction=True,
+                   applyflags=True,
+                   overwrite=False,
+                   )
+        self.msname = msname
+        return
 
     def _initialize_metadata(self):
         # Fetch metadata from ms
@@ -198,37 +220,34 @@ class CalObject:
         self.minspw = str(np.min(spw_list))
         self.maxspw = str(np.max(spw_list))
         self.spwrange = self.minspw+'~'+self.maxspw
-        self.quacked_spwstr = self.spwrange+':4~60'
-
-        f_dot, l_dot = search(self.msname)
-        mod_julian_date = self.msname[f_dot:l_dot]
-        julian_date = 2400000.+float(mod_julian_date)
-        self.day = julian_day_to_date(julian_date)
+        self.quacked_spwstr = self.spwrange+f':{self.fchan}~{self.lchan}'
 
     def _report_init(self):
         print('Scans used for calibration:')
         print(self.cal_scans)
+        print()
         print('Scans used for beamcut:')
         print(self.beamcut_scans)
+        print()
         print('SPWSs used for beamcuts:')
         print(self.spwrange)
-        print('Date obtained:')
-        print(self.day)
+        print()
 
     def _do_calibration(self, cal_name):
         if os.path.exists(cal_name):
             print(f'{cal_name} exists.')
             if self.overwrite:
-                print('\r Overwriting it')
+                print(f'{cal_name} exists, overwriting.')
                 return True
             else:
-                print('\r keeping it')
+                print(f'{cal_name} exists, keeping it.')
                 return False
         else:
             print(f'{cal_name} does not exist, creating it...')
             return True
 
     def delay_calibration(self):
+        print(self.msger.one_liner('Delay calibration...'))
         if self._do_calibration(self.delay_caltable):
             gaincal(vis = self.msname,
                     caltable = self.delay_caltable,
@@ -237,9 +256,13 @@ class CalObject:
                     spw = self.quacked_spwstr,
                     scan = self.cal_scans,
                     gaintype = 'K')
+            print(self.msger.done())
+        else:
+            print(self.msger.one_liner('Skipping delay calibration...'))
         return
 
     def bandpass_calibration(self):
+        print(self.msger.one_liner('Bandpass calibration...'))
         if self._do_calibration(self.bandpass_caltable):
             bandpass(vis = self.msname,
                      caltable = self.bandpass_caltable,
@@ -249,9 +272,13 @@ class CalObject:
                      solnorm = True,
                      scan = self.cal_scans,
                      gaintable = [self.delay_caltable])
+            print(self.msger.done())
+        else:
+            print(self.msger.one_liner('Skipping bandpass calibration...'))
         return
 
     def gain_calibration(self):
+        print(self.msger.one_liner('Gain calibration...'))
         if self._do_calibration(self.gain_caltable):
             gaincal(vis=self.msname,
                     caltable=self.gain_caltable,
@@ -263,9 +290,13 @@ class CalObject:
                     minblperant=2,
                     scan=self.cal_scans,
                     gaintable=[self.delay_caltable, self.bandpass_caltable])
+            print(self.msger.done())
+        else:
+            print(self.msger.one_liner('Skipping gain calibration...'))
         return
 
     def apply_calibration(self):
+        print(self.msger.one_liner('Applying calibration...'))
         applycal(vis=self.msname,
                  field=self.field,
                  spw=self.quacked_spwstr,
@@ -274,6 +305,7 @@ class CalObject:
                             self.bandpass_caltable,
                             self.gain_caltable]
                  )
+        print(self.msger.done())
         return
 
     def calibration_pipeline(self):

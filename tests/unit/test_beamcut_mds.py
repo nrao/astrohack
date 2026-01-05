84a802e27f86d92e72b41ac8c61d5b889642db75
Victor de Souza magalhaes <vdesouza@nrao.edu>
2026-01-05 13:18:24 -0700

    Added tests for init, open, keys, get and set item methods.

--- tests/unit/test_beamcut_mds.py
+++ tests/unit/test_beamcut_mds.py
@@ -1,21 +1,26 @@
 import pytest
 import shutil
+import xarray
+
+from collections.abc import KeysView
 
 from toolviper.utils import data
 
-from astrohack import open_beamcut
+from astrohack import open_beamcut, AstrohackBeamcutFile
 
 
 class TestBeamcut:
     data_folder = "beamcut_data"
     destination_folder = "beamcut_exports"
 
-    ms_name = "kband_beamcut_small.ms"
-    point_name = "kband_beamcut_small.point.zarr"
-    holog_name = "kband_beamcut_small.holog.zarr"
-    local_beamcut_name = "kband_beamcut_small_local.beamcut.zarr"
+    silly_name = "azurbanipal"
+    # ms_name = "kband_beamcut_small.ms"
+    # point_name = "kband_beamcut_small.point.zarr"
+    # holog_name = "kband_beamcut_small.holog.zarr"
+    # local_beamcut_name = "kband_beamcut_small_local.beamcut.zarr"
     remote_beamcut_name = "kband_beamcut_small.beamcut.zarr"
-    ea15_report = "beamcut_exports/beamcut_report_ant_ea15_ddi_0.txt"
+    summary_reference_name = "summary_reference.txt"
+    # ea15_report = "beamcut_exports/beamcut_report_ant_ea15_ddi_0.txt"
 
     @classmethod
     def setup_class(cls):
@@ -33,26 +38,56 @@ class TestBeamcut:
     def teardown_class(cls):
         """teardown any state that was previously setup with a call to setup_class
         such as deleting test data"""
-        shutil.rmtree(cls.data_folder, ignore_errors=True)
-        shutil.rmtree(cls.destination_folder, ignore_errors=True)
-
-    def test_init_beamcut(self):
+        # shutil.rmtree(cls.data_folder, ignore_errors=True)
+        # shutil.rmtree(cls.destination_folder, ignore_errors=True)
         return
 
-    def test_open_beamcut(self):
+    def test_init_and_open_beamcut(self):
+        beamcut_mds = AstrohackBeamcutFile(self.silly_name)
+
+        assert (
+            beamcut_mds.file == self.silly_name
+        ), "Beamcut mds file name should be the same as the one given as argument to __init__"
+
+        assert not beamcut_mds.is_open, "Beamcut mds file should not be opened yet"
+
+        sucessful_open = beamcut_mds.open()
+        assert (
+            not sucessful_open
+        ), "opening beamcut_mds file should fail when beamcut_mds.file is set to nonsense"
+
+        sucessful_open = beamcut_mds.open(self.remote_beamcut_name)
+        assert (
+            sucessful_open
+        ), "Opening beamcut should succeed now that the correct file name is given"
+        assert (
+            beamcut_mds.is_open
+        ), "is_open property needs to return True now that the file has been opened"
+        assert (
+            beamcut_mds.file == self.remote_beamcut_name
+        ), ".file attribute should now be set to the name of the given file."
+
         return
 
-    def test_beamcut_setitem(self):
-        return
+    def test_beamcut_keys_getitem_and_setitem(self):
+        beamcut_mds = open_beamcut(self.remote_beamcut_name)
 
-    def test_beamcut_getitem(self):
-        return
+        old_xdt_keys = beamcut_mds.keys()
+        assert isinstance(
+            old_xdt_keys, KeysView
+        ), "Keys method should return a dict_keys object"
+        assert len(old_xdt_keys) == 2, "File should contain 2 antenna subtrees"
 
-    def test_beamcut_open(self):
-        # .is_open to be tested here as well
-        return
+        ant_17_subtree = beamcut_mds["ant_ea17"]
+        assert isinstance(ant_17_subtree, xarray.DataTree)
 
-    def test_beamcut_keys(self):
+        beamcut_mds["ant_ea19"] = ant_17_subtree
+        new_xdt_keys = beamcut_mds.keys()
+        assert len(new_xdt_keys) == 3, "File should now contain 3 antenna subtrees"
+        assert (
+            "ant_ea19" in new_xdt_keys
+        ), "New antenna subtree should appear amongst keys"
+
         return
 
     def test_beamcut_summary(self):

922378b331322ae9f24a9363e4e2dde9c5296126
Victor de Souza magalhaes <vdesouza@nrao.edu>
2026-01-05 11:59:22 -0700

    Added expected tests to test_beamcut_mds.py

--- tests/unit/test_beamcut_mds.py
+++ tests/unit/test_beamcut_mds.py
@@ -1,5 +1,5 @@
 import pytest
-
+import shutil
 
 from toolviper.utils import data
 
@@ -29,4 +29,44 @@ class TestBeamcut:
                 if varname.split("_")[-1] == "name":
                     setattr(cls, varname, f"{cls.data_folder}/{varvalue}")
 
-        cls.beamcut_mds = open_beamcut(cls.remote_beamcut_name)
+    @classmethod
+    def teardown_class(cls):
+        """teardown any state that was previously setup with a call to setup_class
+        such as deleting test data"""
+        shutil.rmtree(cls.data_folder, ignore_errors=True)
+        shutil.rmtree(cls.destination_folder, ignore_errors=True)
+
+    def test_init_beamcut(self):
+        return
+
+    def test_open_beamcut(self):
+        return
+
+    def test_beamcut_setitem(self):
+        return
+
+    def test_beamcut_getitem(self):
+        return
+
+    def test_beamcut_open(self):
+        # .is_open to be tested here as well
+        return
+
+    def test_beamcut_keys(self):
+        return
+
+    def test_beamcut_summary(self):
+        return
+
+    def test_beamcut_observation_summary(self):
+        return
+
+    def test_beamcut_plots(self):
+        # plot_beamcut_in_amplitude
+        # plot_beamcut_in_attenuation
+        # plot_beam_cuts_over_sky
+
+        return
+
+    def test_beam_fit_report(self):
+        return

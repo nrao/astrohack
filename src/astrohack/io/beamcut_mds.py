26354fd6ff78eb73c7cf4ea1c4e0703a86572eec
Victor de Souza Magalhaes <vdesouza@nrao.edu>
2026-01-08 14:21:51 -0700

    all methods in beamcut_mds.py reactivated.

--- src/astrohack/io/beamcut_mds.py
+++ src/astrohack/io/beamcut_mds.py
@@ -46,267 +46,254 @@ class AstrohackBeamcutFile(AstrohackBaseFile):
         """
         super().__init__(file=file)
 
-    # def summary(self) -> None:
-    #     """
-    #     Prints summary of the AstrohackBeamcutFile object, with available data, attributes and available methods
-    #
-    #     :return: None
-    #     :rtype: NoneType
-    #     """
-    #     print_summary_header(self.file)
-    #     print_dict_table(self.xdt.attrs)
-    #     print_data_contents(self, ["Antenna", "DDI", "Cut"])
-    #     print_method_list_xdt(self)
+    @validate(custom_checker=custom_unit_checker)
+    def observation_summary(
+        self,
+        summary_file: str,
+        ant: Union[str, List[str]] = "all",
+        ddi: Union[int, List[int]] = "all",
+        az_el_key: str = "center",
+        phase_center_unit: str = "radec",
+        az_el_unit: str = "deg",
+        time_format: str = "%d %h %Y, %H:%M:%S",
+        tab_size: int = 3,
+        print_summary: bool = True,
+        parallel: bool = False,
+    ) -> None:
+        """
+        Create a Summary of observation information
 
-    #
-    # @validate(custom_checker=custom_unit_checker)
-    # def observation_summary(
-    #     self,
-    #     summary_file: str,
-    #     ant: Union[str, List[str]] = "all",
-    #     ddi: Union[int, List[int]] = "all",
-    #     az_el_key: str = "center",
-    #     phase_center_unit: str = "radec",
-    #     az_el_unit: str = "deg",
-    #     time_format: str = "%d %h %Y, %H:%M:%S",
-    #     tab_size: int = 3,
-    #     print_summary: bool = True,
-    #     parallel: bool = False,
-    # ) -> None:
-    #     """
-    #     Create a Summary of observation information
-    #
-    #     :param summary_file: Text file to put the observation summary
-    #     :type summary_file: str
-    #
-    #     :param ant: antenna ID to use in subselection, defaults to "all" when None, ex. ea25
-    #     :type ant: list or str, optional
-    #
-    #     :param ddi: data description ID to use in subselection, defaults to "all" when None, ex. 0
-    #     :type ddi: list or int, optional
-    #
-    #     :param az_el_key: What type of Azimuth & Elevation information to print, 'mean', 'median' or 'center', default\
-    #     is 'center'
-    #     :type az_el_key: str, optional
-    #
-    #     :param phase_center_unit: What unit to display phase center coordinates, 'radec' and angle units supported, \
-    #     default is 'radec'
-    #     :type phase_center_unit: str, optional
-    #
-    #     :param az_el_unit: Angle unit used to display Azimuth & Elevation information, default is 'deg'
-    #     :type az_el_unit: str, optional
-    #
-    #     :param time_format: datetime time format for the start and end dates of observation, default is \
-    #     "%d %h %Y, %H:%M:%S"
-    #     :type time_format: str, optional
-    #
-    #     :param tab_size: Number of spaces in the tab levels, default is 3
-    #     :type tab_size: int, optional
-    #
-    #     :param print_summary: Print the summary at the end of execution, default is True
-    #     :type print_summary: bool, optional
-    #
-    #     :param parallel: Run in parallel, defaults to False
-    #     :type parallel: bool, optional
-    #
-    #     :return: None
-    #     :rtype: NoneType
-    #
-    #     **Additional Information**
-    #
-    #     This method produces a summary of the data in the AstrohackBeamcutFile displaying general information,
-    #     spectral information, beam image characteristics and aperture image characteristics.
-    #     """
-    #
-    #     param_dict = locals()
-    #     key_order = ["ant", "ddi"]
-    #     execution, summary_list = compute_graph(
-    #         self,
-    #         generate_observation_summary_for_beamcut,
-    #         param_dict,
-    #         key_order,
-    #         parallel,
-    #         fetch_returns=True,
-    #     )
-    #     full_summary = "".join(summary_list)
-    #     with open(summary_file, "w") as output_file:
-    #         output_file.write(full_summary)
-    #     if print_summary:
-    #         print(full_summary)
-    #
-    # @validate(custom_checker=custom_plots_checker)
-    # def plot_beamcut_in_amplitude(
-    #     self,
-    #     destination: str,
-    #     ant: Union[str, List[str]] = "all",
-    #     ddi: Union[int, List[int]] = "all",
-    #     lm_unit: str = "amin",
-    #     azel_unit: str = "deg",
-    #     y_scale: list[float] = None,
-    #     display: bool = False,
-    #     dpi: int = 300,
-    #     parallel: bool = False,
-    # ) -> None:
-    #     """
-    #     Plot beamcuts contained in the beamcut_mds in amplitude
-    #
-    #     :param destination: Directory into which to save plots.
-    #     :type destination: str
-    #
-    #     :param ant: Antenna ID to use in subselection, e.g. ea25, defaults to "all".
-    #     :type ant: list or str, optional
-    #
-    #     :param ddi: Data description ID to use in subselection, e.g. 0, defaults to "all".
-    #     :type ddi: list or int, optional
-    #
-    #     :param lm_unit: Unit for L/M offsets, default is "amin".
-    #     :type lm_unit: str, optional
-    #
-    #     :param azel_unit: Unit for Az/El information, default is "deg".
-    #     :type azel_unit: str, optional
-    #
-    #     :param y_scale: Set the y scale for the plots.
-    #     :type y_scale: str, optional
-    #
-    #     :param display: Display plots during execution, default is False.
-    #     :type display: bool, optional
-    #
-    #     :param dpi: Pixel resolution for plots, default is 300.
-    #     :type dpi: int, optional
-    #
-    #     :param parallel: Run in parallel, defaults to False.
-    #     :type parallel: bool, optional
-    #
-    #     :return: None
-    #     :rtype: NoneType
-    #     """
-    #
-    #     param_dict = locals()
-    #
-    #     pathlib.Path(param_dict["destination"]).mkdir(exist_ok=True)
-    #     compute_graph(
-    #         self,
-    #         plot_beamcut_in_amplitude_chunk,
-    #         param_dict,
-    #         ["ant", "ddi"],
-    #         parallel=parallel,
-    #     )
-    #     return
-    #
-    # @validate(custom_checker=custom_plots_checker)
-    # def plot_beamcut_in_attenuation(
-    #     self,
-    #     destination: str,
-    #     ant: Union[str, List[str]] = "all",
-    #     ddi: Union[int, List[int]] = "all",
-    #     lm_unit: str = "amin",
-    #     azel_unit: str = "deg",
-    #     y_scale: str = None,
-    #     display: bool = False,
-    #     dpi: int = 300,
-    #     parallel: bool = False,
-    # ) -> None:
-    #     """
-    #     Plot beamcuts contained in the beamcut_mds in attenuation
-    #
-    #     :param destination: Directory into which to save plots.
-    #     :type destination: str
-    #
-    #     :param ant: Antenna ID to use in subselection, e.g. ea25, defaults to "all".
-    #     :type ant: list or str, optional
-    #
-    #     :param ddi: Data description ID to use in subselection, e.g. 0, defaults to "all".
-    #     :type ddi: list or int, optional
-    #
-    #     :param lm_unit: Unit for L/M offsets, default is "amin".
-    #     :type lm_unit: str, optional
-    #
-    #     :param azel_unit: Unit for Az/El information, default is "deg".
-    #     :type azel_unit: str, optional
-    #
-    #     :param y_scale: Set the y scale for the plots.
-    #     :type y_scale: str, optional
-    #
-    #     :param display: Display plots during execution, default is False.
-    #     :type display: bool, optional
-    #
-    #     :param dpi: Pixel resolution for plots, default is 300.
-    #     :type dpi: int, optional
-    #
-    #     :param parallel: Run in parallel, defaults to False.
-    #     :type parallel: bool, optional
-    #
-    #     :return: None
-    #     :rtype: NoneType
-    #     """
-    #
-    #     param_dict = locals()
-    #
-    #     pathlib.Path(param_dict["destination"]).mkdir(exist_ok=True)
-    #     compute_graph(
-    #         self,
-    #         plot_beamcut_in_attenuation_chunk,
-    #         param_dict,
-    #         ["ant", "ddi"],
-    #         parallel=parallel,
-    #     )
-    #     return
-    #
-    # @validate(custom_checker=custom_plots_checker)
-    # def plot_beam_cuts_over_sky(
-    #     self,
-    #     destination: str,
-    #     ant: Union[str, List[str]] = "all",
-    #     ddi: Union[int, List[int]] = "all",
-    #     lm_unit: str = "amin",
-    #     azel_unit: str = "deg",
-    #     display: bool = False,
-    #     dpi: int = 300,
-    #     parallel: bool = False,
-    # ) -> None:
-    #     """
-    #     Plot beamcuts contained in the beamcut_mds over the sky
-    #
-    #     :param destination: Directory into which to save plots.
-    #     :type destination: str
-    #
-    #     :param ant: Antenna ID to use in subselection, e.g. ea25, defaults to "all".
-    #     :type ant: list or str, optional
-    #
-    #     :param ddi: Data description ID to use in subselection, e.g. 0, defaults to "all".
-    #     :type ddi: list or int, optional
-    #
-    #     :param lm_unit: Unit for L/M offsets, default is "amin".
-    #     :type lm_unit: str, optional
-    #
-    #     :param azel_unit: Unit for Az/El information, default is "deg".
-    #     :type azel_unit: str, optional
-    #
-    #     :param display: Display plots during execution, default is False.
-    #     :type display: bool, optional
-    #
-    #     :param dpi: Pixel resolution for plots, default is 300.
-    #     :type dpi: int, optional
-    #
-    #     :param parallel: Run in parallel, defaults to False.
-    #     :type parallel: bool, optional
-    #
-    #     :return: None
-    #     :rtype: NoneType
-    #     """
-    #
-    #     param_dict = locals()
-    #
-    #     pathlib.Path(param_dict["destination"]).mkdir(exist_ok=True)
-    #     compute_graph(
-    #         self,
-    #         plot_cuts_in_lm_chunk,
-    #         param_dict,
-    #         ["ant", "ddi"],
-    #         parallel=parallel,
-    #     )
-    #     return
-    #
+        :param summary_file: Text file to put the observation summary
+        :type summary_file: str
+
+        :param ant: antenna ID to use in subselection, defaults to "all" when None, ex. ea25
+        :type ant: list or str, optional
+
+        :param ddi: data description ID to use in subselection, defaults to "all" when None, ex. 0
+        :type ddi: list or int, optional
+
+        :param az_el_key: What type of Azimuth & Elevation information to print, 'mean', 'median' or 'center', default\
+        is 'center'
+        :type az_el_key: str, optional
+
+        :param phase_center_unit: What unit to display phase center coordinates, 'radec' and angle units supported, \
+        default is 'radec'
+        :type phase_center_unit: str, optional
+
+        :param az_el_unit: Angle unit used to display Azimuth & Elevation information, default is 'deg'
+        :type az_el_unit: str, optional
+
+        :param time_format: datetime time format for the start and end dates of observation, default is \
+        "%d %h %Y, %H:%M:%S"
+        :type time_format: str, optional
+
+        :param tab_size: Number of spaces in the tab levels, default is 3
+        :type tab_size: int, optional
+
+        :param print_summary: Print the summary at the end of execution, default is True
+        :type print_summary: bool, optional
+
+        :param parallel: Run in parallel, defaults to False
+        :type parallel: bool, optional
+
+        :return: None
+        :rtype: NoneType
+
+        **Additional Information**
+
+        This method produces a summary of the data in the AstrohackBeamcutFile displaying general information,
+        spectral information, beam image characteristics and aperture image characteristics.
+        """
+
+        param_dict = locals()
+        key_order = ["ant", "ddi"]
+        execution, summary_list = compute_graph(
+            self,
+            generate_observation_summary_for_beamcut,
+            param_dict,
+            key_order,
+            parallel,
+            fetch_returns=True,
+        )
+        full_summary = "".join(summary_list)
+        with open(summary_file, "w") as output_file:
+            output_file.write(full_summary)
+        if print_summary:
+            print(full_summary)
+
+    @validate(custom_checker=custom_plots_checker)
+    def plot_beamcut_in_amplitude(
+        self,
+        destination: str,
+        ant: Union[str, List[str]] = "all",
+        ddi: Union[int, List[int]] = "all",
+        lm_unit: str = "amin",
+        azel_unit: str = "deg",
+        y_scale: list[float] = None,
+        display: bool = False,
+        dpi: int = 300,
+        parallel: bool = False,
+    ) -> None:
+        """
+        Plot beamcuts contained in the beamcut_mds in amplitude
+
+        :param destination: Directory into which to save plots.
+        :type destination: str
+
+        :param ant: Antenna ID to use in subselection, e.g. ea25, defaults to "all".
+        :type ant: list or str, optional
+
+        :param ddi: Data description ID to use in subselection, e.g. 0, defaults to "all".
+        :type ddi: list or int, optional
+
+        :param lm_unit: Unit for L/M offsets, default is "amin".
+        :type lm_unit: str, optional
+
+        :param azel_unit: Unit for Az/El information, default is "deg".
+        :type azel_unit: str, optional
+
+        :param y_scale: Set the y scale for the plots.
+        :type y_scale: str, optional
+
+        :param display: Display plots during execution, default is False.
+        :type display: bool, optional
+
+        :param dpi: Pixel resolution for plots, default is 300.
+        :type dpi: int, optional
+
+        :param parallel: Run in parallel, defaults to False.
+        :type parallel: bool, optional
+
+        :return: None
+        :rtype: NoneType
+        """
+
+        param_dict = locals()
+
+        pathlib.Path(param_dict["destination"]).mkdir(exist_ok=True)
+        compute_graph(
+            self,
+            plot_beamcut_in_amplitude_chunk,
+            param_dict,
+            ["ant", "ddi"],
+            parallel=parallel,
+        )
+        return
+
+    @validate(custom_checker=custom_plots_checker)
+    def plot_beamcut_in_attenuation(
+        self,
+        destination: str,
+        ant: Union[str, List[str]] = "all",
+        ddi: Union[int, List[int]] = "all",
+        lm_unit: str = "amin",
+        azel_unit: str = "deg",
+        y_scale: str = None,
+        display: bool = False,
+        dpi: int = 300,
+        parallel: bool = False,
+    ) -> None:
+        """
+        Plot beamcuts contained in the beamcut_mds in attenuation
+
+        :param destination: Directory into which to save plots.
+        :type destination: str
+
+        :param ant: Antenna ID to use in subselection, e.g. ea25, defaults to "all".
+        :type ant: list or str, optional
+
+        :param ddi: Data description ID to use in subselection, e.g. 0, defaults to "all".
+        :type ddi: list or int, optional
+
+        :param lm_unit: Unit for L/M offsets, default is "amin".
+        :type lm_unit: str, optional
+
+        :param azel_unit: Unit for Az/El information, default is "deg".
+        :type azel_unit: str, optional
+
+        :param y_scale: Set the y scale for the plots.
+        :type y_scale: str, optional
+
+        :param display: Display plots during execution, default is False.
+        :type display: bool, optional
+
+        :param dpi: Pixel resolution for plots, default is 300.
+        :type dpi: int, optional
+
+        :param parallel: Run in parallel, defaults to False.
+        :type parallel: bool, optional
+
+        :return: None
+        :rtype: NoneType
+        """
+
+        param_dict = locals()
+
+        pathlib.Path(param_dict["destination"]).mkdir(exist_ok=True)
+        compute_graph(
+            self,
+            plot_beamcut_in_attenuation_chunk,
+            param_dict,
+            ["ant", "ddi"],
+            parallel=parallel,
+        )
+        return
+
+    @validate(custom_checker=custom_plots_checker)
+    def plot_beam_cuts_over_sky(
+        self,
+        destination: str,
+        ant: Union[str, List[str]] = "all",
+        ddi: Union[int, List[int]] = "all",
+        lm_unit: str = "amin",
+        azel_unit: str = "deg",
+        display: bool = False,
+        dpi: int = 300,
+        parallel: bool = False,
+    ) -> None:
+        """
+        Plot beamcuts contained in the beamcut_mds over the sky
+
+        :param destination: Directory into which to save plots.
+        :type destination: str
+
+        :param ant: Antenna ID to use in subselection, e.g. ea25, defaults to "all".
+        :type ant: list or str, optional
+
+        :param ddi: Data description ID to use in subselection, e.g. 0, defaults to "all".
+        :type ddi: list or int, optional
+
+        :param lm_unit: Unit for L/M offsets, default is "amin".
+        :type lm_unit: str, optional
+
+        :param azel_unit: Unit for Az/El information, default is "deg".
+        :type azel_unit: str, optional
+
+        :param display: Display plots during execution, default is False.
+        :type display: bool, optional
+
+        :param dpi: Pixel resolution for plots, default is 300.
+        :type dpi: int, optional
+
+        :param parallel: Run in parallel, defaults to False.
+        :type parallel: bool, optional
+
+        :return: None
+        :rtype: NoneType
+        """
+
+        param_dict = locals()
+
+        pathlib.Path(param_dict["destination"]).mkdir(exist_ok=True)
+        compute_graph(
+            self,
+            plot_cuts_in_lm_chunk,
+            param_dict,
+            ["ant", "ddi"],
+            parallel=parallel,
+        )
+        return
+
     @validate(custom_checker=custom_plots_checker)
     def create_beam_fit_report(
         self,

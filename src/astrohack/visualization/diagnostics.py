a84b61da30f7520d19d8c52a5b385c9aeab53463
Victor de Souza Magalhaes <vdesouza@nrao.edu>
2026-02-03 11:12:11 -0700

    Plot_array_configuration can now chooses an internal box of 0.2 times the size of the array when no box size is given.

--- src/astrohack/visualization/diagnostics.py
+++ src/astrohack/visualization/diagnostics.py
@@ -684,7 +684,7 @@ def _plot_zernike_aperture_model(
     )
 
 
-def plot_array_configuration(input_dict, xdtree, caller):
+def plot_array_configuration(input_dict, xdtree, caller, box_default_size=0.2):
     telescope_name = xdtree.attrs["telescope_name"]
     telescope = get_proper_telescope(telescope_name)
     stations = input_dict["stations"]
@@ -696,15 +696,10 @@ def plot_array_configuration(input_dict, xdtree, caller):
     box_size = input_dict["box_size"]  # In user input unit
     plot_zoff = input_dict["zoff"]
 
-    fig, axes = create_figure_and_axes(figure_size, [1, 2], default_figsize=[10, 5])
-
     len_fac = convert_unit("m", length_unit, "length")
-
-    inner_ax = axes[1]
-    outer_ax = axes[0]
-
     tel_lon, tel_lat, tel_rad = get_telescope_lat_lon_rad(telescope)
-
+    ant_offs = []
+    ant_texts = []
     for ant_xdtree in xdtree.values():
         ant_info = ant_xdtree.attrs["antenna_info"]
         ew_off, ns_off, el_off, _ = compute_antenna_relative_off(
@@ -715,6 +710,22 @@ def plot_array_configuration(input_dict, xdtree, caller):
             text += f'@{ant_info["station"]}'
         if plot_zoff:
             text += f" {el_off:.1f} {length_unit}"
+        ant_offs.append([ew_off, ns_off])
+        ant_texts.append(text)
+
+    ant_offs = np.array(ant_offs)
+    if box_size is None:
+        ew_min, ew_max = np.min(ant_offs[:, 0]), np.max(ant_offs[:, 0])
+        ew_range = ew_max - ew_min
+        ns_min, ns_max = np.min(ant_offs[:, 1]), np.max(ant_offs[:, 1])
+        ns_range = ns_max - ns_min
+        box_size = box_default_size * np.max([ew_range, ns_range])
+
+    fig, axes = create_figure_and_axes(figure_size, [1, 2], default_figsize=[10, 5])
+    inner_ax = axes[1]
+    outer_ax = axes[0]
+    for i_ant, text in enumerate(ant_texts):
+        ew_off, ns_off = ant_offs[i_ant]
         plot_one_antenna_position(outer_ax, inner_ax, ew_off, ns_off, text, box_size)
 
     # axes labels

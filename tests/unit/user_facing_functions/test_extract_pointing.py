ddd582a543f8052650b84f108aa0037cc3d95efa
Victor de Souza Magalhaes <vdesouza@nrao.edu>
2026-02-11 10:28:35 -0700

    rewrote test_extract_pointing.py

--- tests/unit/user_facing_functions/test_extract_pointing.py
+++ tests/unit/user_facing_functions/test_extract_pointing.py
@@ -4,6 +4,7 @@ import shutil
 import pytest
 import toolviper
 
+from astrohack import open_pointing
 from astrohack.extract_pointing import extract_pointing
 from astrohack.utils.verification_tools import add_data_folder_to_names_in_class
 
@@ -19,85 +20,62 @@ class TestExtractPointing:
     def setup_class(cls):
         """setup any state specific to the execution of the given test class"""
         toolviper.utils.data.download(file=cls.ms_name, folder=cls.data_dir)
+        toolviper.utils.data.download(file=cls.ref_pnt_name, folder=cls.data_dir)
         add_data_folder_to_names_in_class(cls)
 
     @classmethod
     def teardown_class(cls):
         """teardown any state that was previously setup with a setup_class."""
-        # shutil.rmtree(cls.data_dir)
+        shutil.rmtree(cls.data_dir)
 
-    def test_extract_pointing_defaults(self):
+    def test_defaults(self):
         """Test extract_pointing with default parameters"""
+
         new_pnt_mds = extract_pointing(ms_name=self.ms_name, overwrite=True)
-        assert pathlib.Path(self.def_pnt_name).is_dir()
+        assert pathlib.Path(
+            self.def_pnt_name
+        ).is_dir(), f"A .point.zarr file named {self.def_pnt_name} does not exist."
 
+        ref_pnt_mds = open_pointing(self.ref_pnt_name)
+        assert new_pnt_mds == ref_pnt_mds, "Reference and new mdses are different."
 
-# class TestExtractPointing:
-#     data_dir = "point_data"
-#     ms_name = "ea25_cal_small_before_fixed.split.ms"
-#     def_pnt_name = "ea25_cal_small_before_fixed.split.point.zarr"
-#     alt_pnt_name = "ea25_short.point.zarr"
-#     ref_pnt_name = "ea25_cal_small_before_reference.point.zarr"
-#     print("repolho")
+    def test_renaming(self):
+        """Test extract_pointing naming"""
+        new_pnt_mds = extract_pointing(
+            ms_name=self.ms_name, point_name=self.alt_pnt_name, overwrite=True
+        )
 
-# @classmethod
-# def setup_class(cls):
-#     """setup any state specific to the execution of the given test class"""
-#     print("feijao")
-#     toolviper.utils.data.download(file=cls.ms_name, folder=cls.data_dir)
-#     # add_data_folder_to_names_in_class(cls)
-#
-# @classmethod
-# def teardown_class(cls):
-#     """teardown any state that was previously setup with a call to setup_class"""
-#     return
-#     shutil.rmtree(cls.data_dir)
+        assert pathlib.Path(
+            self.alt_pnt_name
+        ).is_dir(), f"A .point.zarr file named {self.alt_pnt_name} does not exist."
 
-# def test_extract_pointing_default(self):
-#     """Test extract_pointing with default parameters"""
-#     print("aaaaa")
-#     print(self.ms_name)
-#     # point_obj = extract_pointing(ms_name=self.ms_name)
-#     # print(point_obj)
-#     assert True
-#     # # Check the keys of the returned dictionary
-#     # expected_keys = ["point_meta_ds", "ant_ea04", "ant_ea06", "ant_ea25"]
-#     #
-#     # for key in point_obj.keys():
-#     #     assert key in expected_keys
+        assert (
+            new_pnt_mds.filename == self.alt_pnt_name
+        ), "Point mds filename does not match the file on disk."
 
-# @pytest.mark.skip(reason="mds equality test is not yet robust")
-# def test_extract_pointing_point_name(self):
-#     """Test extract_pointing and save to given point name"""
-#     point_name = os.path.join(self.data_dir, "test_user_point_name.zarr")
-#     point_obj = extract_pointing(ms_name=self.ms_name, point_name=point_name)
-#
-#     assert os.path.exists(point_name)
-#
-#     # Check that the returned dictionary contains the given point_name
-#     assert point_obj.file == point_name
-#
-# @pytest.mark.skip(reason="mds equality test is not yet robust")
-# def test_extract_pointing_overwrite_true(self):
-#     """Test extract_pointing and overwrite existing pointing file"""
-#     point_name = os.path.join(self.data_dir, "test_user_overwrite.zarr")
-#     extract_pointing(ms_name=self.ms_name, point_name=point_name)
-#     initial_time = os.path.getctime(point_name)
-#
-#     extract_pointing(ms_name=self.ms_name, point_name=point_name, overwrite=True)
-#     modified_time = os.path.getctime(point_name)
-#
-#     assert initial_time != modified_time
-#
-# @pytest.mark.skip(reason="mds equality test is not yet robust")
-# def test_extract_pointing_invalid_ms_name(self):
-#     """Test extract_pointing and check that invalid_ms does not create point file"""
-#     # Exceptions are not raised by the code, therefore doing a silly check here
-#     try:
-#         # Changed this because it does fail if you give a non-existent ms. So we will check that.
-#         extract_pointing(ms_name="invalid_name.ms")
-#
-#     except Exception:
-#         return
-#
-#     assert False
+        ref_pnt_mds = open_pointing(self.ref_pnt_name)
+        assert new_pnt_mds == ref_pnt_mds, "Reference and new mdses are different."
+
+    def test_antenna_exclusion(self):
+        """Test extract_pointing antenna exclusion"""
+        excluded_antenna = "ea06"
+        new_pnt_mds = extract_pointing(
+            ms_name=self.ms_name, exclude=excluded_antenna, overwrite=True
+        )
+
+        assert (
+            f"ant_{excluded_antenna}" not in new_pnt_mds.keys()
+        ), "Excluded antenna should not be present in the new mds."
+
+    def test_invalid_ms_name(self):
+        """Test extract_pointing with invalid ms name"""
+        bogus_ms = "non-existe.ms"
+        with pytest.raises(FileNotFoundError):
+            extract_pointing(ms_name=bogus_ms, overwrite=True)
+
+    def test_overwrite(self):
+        """Test extract_pointing overwrite behaviour"""
+        with pytest.raises(FileExistsError):
+            extract_pointing(
+                ms_name=self.ms_name, point_name=self.ref_pnt_name, overwrite=False
+            )

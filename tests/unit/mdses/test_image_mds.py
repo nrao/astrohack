dac1e64e858e1c2614000435fd5cf2117bde5d8d
Victor de Souza Magalhaes <vdesouza@nrao.edu>
2026-02-20 15:56:28 -0700

    Implemented test_image_mds.py.

---
+++ tests/unit/mdses/test_image_mds.py
@@ -0,0 +1,139 @@
+import glob
+import shutil
+import os
+import pathlib
+import matplotlib
+
+from toolviper.utils import data
+
+from astrohack import AstrohackImageFile, open_image
+from astrohack.utils.verification_tools import (
+    add_data_folder_to_names_in_class,
+    are_txt_files_equal,
+    are_png_files_close,
+    are_fits_files_close,
+)
+
+matplotlib.use("Agg")
+
+
+class TestimageMDS:
+    data_dir = "image_data"
+    destination_folder = "image_exports"
+    ref_products_name = f"ref_image_products"
+
+    img_name = "ea25_cal_before_reference.image.zarr"
+
+    ant_id = "ea25"
+    ant_key = f"ant_{ant_id}"
+    ddi_id = 0
+    ddi_key = f"ddi_{ddi_id}"
+
+    def plot_list_assertions(self, pattern):
+        plot_name_list = [
+            os.path.basename(full_path_name)
+            for full_path_name in glob.glob(f"{self.ref_products_name}/{pattern}")
+        ]
+        for plot_name in plot_name_list:
+            assert pathlib.Path(
+                f"{self.destination_folder}/{plot_name}"
+            ).is_file(), f"{plot_name} was not created"
+            assert are_png_files_close(
+                f"{self.destination_folder}/{plot_name}",
+                f"{self.ref_products_name}/{plot_name}",
+            ), f"{plot_name} Differs from reference"
+
+    def fits_list_assertions(self):
+        fits_list = [
+            os.path.basename(full_path_name)
+            for full_path_name in glob.glob(f"{self.ref_products_name}/*.fits")
+        ]
+        for fits_name in fits_list:
+            assert pathlib.Path(
+                f"{self.destination_folder}/{fits_name}"
+            ).is_file(), f"{fits_name} was not created"
+            assert are_fits_files_close(
+                f"{self.destination_folder}/{fits_name}",
+                f"{self.ref_products_name}/{fits_name}",
+            ), f"{fits_name} is different from reference"
+
+    @classmethod
+    def setup_class(cls):
+        """setup any state specific to the execution of the given test class
+        such as fetching test data"""
+        data.download(file=cls.img_name, folder=cls.data_dir)
+        data.download(file=cls.ref_products_name, folder=cls.data_dir)
+
+        add_data_folder_to_names_in_class(cls)
+
+    @classmethod
+    def teardown_class(cls):
+        shutil.rmtree(cls.data_dir)
+        shutil.rmtree(cls.destination_folder)
+        return
+
+    def test_init(self):
+        img_mds = AstrohackImageFile(self.img_name)
+        assert isinstance(img_mds, AstrohackImageFile)
+
+    def test_fits_exports(self):
+        img_mds = open_image(self.img_name)
+        img_mds.export_to_fits(
+            self.destination_folder, ant=self.ant_id, ddi=self.ddi_id, parallel=False
+        )
+        self.fits_list_assertions()
+        return
+
+    def test_text_exports(self):
+        img_mds = open_image(self.img_name)
+        img_mds.export_phase_fit_results(
+            self.destination_folder, ant=self.ant_id, ddi=self.ddi_id, parallel=False
+        )
+        txt_file_name = "image_phase_fit_ant_ea25_ddi_0.txt"
+        assert are_txt_files_equal(
+            f"{self.destination_folder}/{txt_file_name}",
+            f"{self.ref_products_name}/{txt_file_name}",
+        ), "Phase fit results are different from reference"
+
+        img_mds.export_zernike_fit_results(
+            self.destination_folder, ant=self.ant_id, ddi=self.ddi_id, parallel=False
+        )
+        txt_file_name = "image_zernike_fit_ant_ea25_ddi_0.txt"
+        assert are_txt_files_equal(
+            f"{self.destination_folder}/{txt_file_name}",
+            f"{self.ref_products_name}/{txt_file_name}",
+        ), "Zernike fit results are different from reference"
+
+        obs_summ_file_name = "obs_summ.txt"
+        img_mds.observation_summary(
+            f"{self.destination_folder}/{obs_summ_file_name}",
+            ant=self.ant_id,
+            ddi=self.ddi_id,
+            parallel=False,
+            print_summary=False,
+        )
+        assert are_txt_files_equal(
+            f"{self.destination_folder}/{obs_summ_file_name}",
+            f"{self.ref_products_name}/{obs_summ_file_name}",
+        ), "Observation summary is different from reference"
+
+        return
+
+    def test_plot_exports(self):
+        img_mds = open_image(self.img_name)
+
+        img_mds.plot_apertures(
+            self.destination_folder, ant=self.ant_id, ddi=self.ddi_id, parallel=False
+        )
+        self.plot_list_assertions("image_aperture_*.png")
+
+        img_mds.plot_beams(
+            self.destination_folder, ant=self.ant_id, ddi=self.ddi_id, parallel=False
+        )
+        self.plot_list_assertions("image_beam_*.png")
+
+        img_mds.plot_zernike_model(
+            self.destination_folder, ant=self.ant_id, ddi=self.ddi_id, parallel=False
+        )
+        self.plot_list_assertions("image_zernike_model_*.png")
+        return

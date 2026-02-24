a20de130a6a581fbf26ef44f5a0c83a7816708f8
Victor de Souza magalhaes <vdesouza@nrao.edu>
2026-02-24 12:19:17 -0700

    Added test for point_mds.set_antennas_as_reference.

--- tests/unit/mdses/test_point_mds.py
+++ tests/unit/mdses/test_point_mds.py
@@ -1,12 +1,14 @@
 import shutil
 import matplotlib
+import numpy as np
 
 from toolviper.utils import data
 
-from astrohack import AstrohackPointFile, open_pointing
+from astrohack import AstrohackPointFile, open_pointing, generate_holog_obs_dict
 from astrohack.utils.verification_tools import (
     add_data_folder_to_names_in_class,
     are_png_files_close,
+    are_lists_equal,
 )
 
 matplotlib.use("Agg")
@@ -21,6 +23,8 @@ class TestPointMDS:
 
     ant_id = "ea25"
     ant_key = f"ant_{ant_id}"
+    alt_ant_id = "ea06"
+    alt_ant_key = f"ant_{alt_ant_id}"
 
     @classmethod
     def setup_class(cls):
@@ -70,3 +74,51 @@ class TestPointMDS:
             f"{self.destination_folder}/{plot_name}",
             f"{self.ref_products_name}/{plot_name}",
         ), "All antennas combined directional cosines plot is failing closeness test"
+
+    def compute_simple_hash(self, pnt_mds: AstrohackPointFile, key_to_hash):
+        return np.sum(np.abs(pnt_mds[self.ant_key][key_to_hash].values))
+
+    def test_set_antennas_as_reference(self):
+        pnt_mds = open_pointing(self.pnt_name)
+
+        old_alt_xdt = pnt_mds[self.alt_ant_key]
+
+        old_dir_hash = self.compute_simple_hash(pnt_mds, "DIRECTION")
+        old_tgt_hash = self.compute_simple_hash(pnt_mds, "TARGET")
+
+        pnt_mds.set_antennas_as_reference(self.ant_id)
+
+        new_dir_hash = self.compute_simple_hash(pnt_mds, "DIRECTION")
+        new_tgt_hash = self.compute_simple_hash(pnt_mds, "TARGET")
+        new_pnt_off_hash = self.compute_simple_hash(pnt_mds, "POINTING_OFFSET")
+        new_dir_cos_hash = self.compute_simple_hash(pnt_mds, "DIRECTIONAL_COSINES")
+
+        assert old_dir_hash == new_dir_hash, "Direction hash should not change"
+        assert old_tgt_hash != new_tgt_hash, "Target hash should have changed"
+        assert (
+            new_tgt_hash == new_dir_hash
+        ), "Target and direction hashes should be equal"
+        assert new_pnt_off_hash == 0, "New pointing offset hash should be zero"
+        assert new_dir_cos_hash == 0, "New directional cosines hash should be zero"
+
+        new_alt_xdt = pnt_mds[self.alt_ant_key]
+
+        assert new_alt_xdt == old_alt_xdt, "Other antenna Data tree should be unchanged"
+
+        hlg_obs_dict = generate_holog_obs_dict(self.pnt_name)
+        exp_ref_ants = ["ea04", self.ant_id]
+
+        for ddi_key, ddi_dict in hlg_obs_dict.items():
+            ant_dict = ddi_dict["map_0"]["ant"]
+            ant_keys = list(ant_dict.keys())
+            assert (
+                len(ant_keys) == 1
+            ), f"There should be a single mapping antenna for {ddi_key}"
+            assert (
+                ant_keys[0] == self.alt_ant_id
+            ), f"The only mapping antenna present for {ddi_key} should be {self.alt_ant_id}"
+
+            ref_ants = ant_dict[self.alt_ant_id]
+            assert are_lists_equal(
+                ref_ants, exp_ref_ants
+            ), f"Reference antennas should be {exp_ref_ants} but got {ref_ants}"

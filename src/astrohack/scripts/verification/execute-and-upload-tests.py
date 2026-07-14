cfebe9379bc6b6ffa588cbb09c90f9ec4100b7bf
Victor de Souza Magalhaes <vdesouza@nrao.edu>
2026-07-14 10:33:14 -0600

    Tests are now fetched from a list.

--- src/astrohack/scripts/verification/execute-and-upload-tests.py
+++ src/astrohack/scripts/verification/execute-and-upload-tests.py
@@ -9,66 +9,66 @@ from astrohack.utils.text import format_duration
 
 def set_tests_to_update():
     test_dict = {
-        # "unit/mdses/test_beamcut_mds.py": {
-        #     "n_items": 1,
-        #     "item_0": {
-        #         "creation": "beamcut_exports",
-        #         "destiny": "ref_beamcut_products",
-        #         "description": "beamcut reference export products",
-        #         "type": "Beam cut",
-        #         "telescope": "VLA",
-        #         "update_manifest": False,
-        #     },
-        #     "cleanup_names": ["beamcut_data"],
-        # },
-        # "unit/mdses/test_image_mds.py": {
-        #     "n_items": 1,
-        #     "item_0": {
-        #         "creation": "image_exports",
-        #         "destiny": "ref_image_products",
-        #         "description": "image export products",
-        #         "type": "Holography",
-        #         "telescope": "VLA",
-        #         "update_manifest": False,
-        #     },
-        #     "cleanup_names": ["image_data"],
-        # },
-        # "unit/mdses/test_locit_mds.py": {
-        #     "n_items": 1,
-        #     "item_0": {
-        #         "creation": "locit_exports",
-        #         "destiny": "ref_locit_products",
-        #         "description": "locit reference export products",
-        #         "type": "Antenna position corrections",
-        #         "telescope": "VLA",
-        #         "update_manifest": False,
-        #     },
-        #     "cleanup_names": ["locit_data"],
-        # },
-        # "unit/mdses/test_panel_mds.py": {
-        #     "n_items": 1,
-        #     "item_0": {
-        #         "creation": "panel_exports",
-        #         "destiny": "ref_panel_products",
-        #         "description": "panel export products",
-        #         "type": "Holography",
-        #         "telescope": "VLA",
-        #         "update_manifest": False,
-        #     },
-        #     "cleanup_names": ["panel_data"],
-        # },
-        # "unit/mdses/test_position_mds.py": {
-        #     "n_items": 1,
-        #     "item_0": {
-        #         "creation": "position_exports",
-        #         "destiny": "ref_position_products",
-        #         "description": "position export products",
-        #         "type": "Antenna position corrections",
-        #         "telescope": "VLA",
-        #         "update_manifest": False,
-        #     },
-        #     "cleanup_names": ["position_data"],
-        # },
+        "unit/mdses/test_beamcut_mds.py": {
+            "n_items": 1,
+            "item_0": {
+                "creation": "beamcut_exports",
+                "destiny": "ref_beamcut_products",
+                "description": "beamcut reference export products",
+                "type": "Beam cut",
+                "telescope": "VLA",
+                "update_manifest": False,
+            },
+            "cleanup_names": ["beamcut_data"],
+        },
+        "unit/mdses/test_image_mds.py": {
+            "n_items": 1,
+            "item_0": {
+                "creation": "image_exports",
+                "destiny": "ref_image_products",
+                "description": "image export products",
+                "type": "Holography",
+                "telescope": "VLA",
+                "update_manifest": False,
+            },
+            "cleanup_names": ["image_data"],
+        },
+        "unit/mdses/test_locit_mds.py": {
+            "n_items": 1,
+            "item_0": {
+                "creation": "locit_exports",
+                "destiny": "ref_locit_products",
+                "description": "locit reference export products",
+                "type": "Antenna position corrections",
+                "telescope": "VLA",
+                "update_manifest": False,
+            },
+            "cleanup_names": ["locit_data"],
+        },
+        "unit/mdses/test_panel_mds.py": {
+            "n_items": 1,
+            "item_0": {
+                "creation": "panel_exports",
+                "destiny": "ref_panel_products",
+                "description": "panel export products",
+                "type": "Holography",
+                "telescope": "VLA",
+                "update_manifest": False,
+            },
+            "cleanup_names": ["panel_data"],
+        },
+        "unit/mdses/test_position_mds.py": {
+            "n_items": 1,
+            "item_0": {
+                "creation": "position_exports",
+                "destiny": "ref_position_products",
+                "description": "position export products",
+                "type": "Antenna position corrections",
+                "telescope": "VLA",
+                "update_manifest": False,
+            },
+            "cleanup_names": ["position_data"],
+        },
         "unit/user_facing_functions/test_beamcut.py": {
             "n_items": 1,
             "item_0": {
@@ -81,18 +81,18 @@ def set_tests_to_update():
             },
             "cleanup_names": ["beamcut_data"],
         },
-        # "unit/user_facing_functions/test_extract_holog.py": {
-        #     "n_items": 1,
-        #     "item_0": {
-        #         "creation": "ext_holog_data/ea25_cal_small_before_fixed.split.holog.zarr",
-        #         "destiny": "ea25_cal_small_before_reference.holog.zarr",
-        #         "description": "Reference Astrohack holog file",
-        #         "type": "Holography",
-        #         "telescope": "VLA",
-        #         "update_manifest": False,
-        #     },
-        #     "cleanup_names": ["ext_holog_data"],
-        # },
+        "unit/user_facing_functions/test_extract_holog.py": {
+            "n_items": 1,
+            "item_0": {
+                "creation": "ext_holog_data/ea25_cal_small_before_fixed.split.holog.zarr",
+                "destiny": "ea25_cal_small_before_reference.holog.zarr",
+                "description": "Reference Astrohack holog file",
+                "type": "Holography",
+                "telescope": "VLA",
+                "update_manifest": False,
+            },
+            "cleanup_names": ["ext_holog_data"],
+        },
     }
 
     return test_dict
@@ -135,16 +135,21 @@ def main():
     import time
 
     wait_time = 3600
-
     distro_path = get_astrohack_path()
     os.chdir(distro_path / "../../tests")
+
+    tests_to_execute = [
+        # "unit/user_facing_functions/test_extract_holog.py",
+        "unit/user_facing_functions/test_holog.py",
+    ]
 
     os.environ["SKIP_PYTEST_CLEANUP"] = "True"
     os.environ["PRODUCE_REFERENCE_PRODUCTS"] = "True"
     test_dict = set_tests_to_update()
-    for test_file, test_params in test_dict.items():
+    for test_file in tests_to_execute:
+        test_params = test_dict[test_file]
         execute_and_upload(test_file, test_params)
-        if len(test_dict.keys()) > 1:
+        if len(tests_to_execute) > 1:
             print(
                 f"{test_file} done, waiting {format_duration(wait_time)} for cloudflare sync before executing next test."
             )

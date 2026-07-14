f45fe7996f807018f6ea9087a419210728104acf
Victor de Souza Magalhaes <vdesouza@nrao.edu>
2026-07-14 10:16:36 -0600

    Live debugging ngvla reduction.

--- src/astrohack/antenna/telescope.py
+++ src/astrohack/antenna/telescope.py
@@ -542,6 +542,7 @@ def get_proper_telescope(name: str, antenna_name: str = None):
     if "ngvla" in name:
         return NgvlaPrototype.from_name("ngvla_proto_2025")
     elif "vla" in name:
+        print(name, antenna_name)
         if antenna_name is None or "ea" in antenna_name or antenna_name == "all":
             return RingedCassegrain.from_name("vla")
         elif "na" in antenna_name:

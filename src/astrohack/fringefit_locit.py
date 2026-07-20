25e20b9021f325de093faa43696d7d8600adebf2
Victor de Souza Magalhaes <vdesouza@nrao.edu>
2026-07-20 15:13:18 -0600

    Made core/extract_locit.py metadata fetching routines more flexible to allow for use in fringefit_locit.py

--- src/astrohack/fringefit_locit.py
+++ src/astrohack/fringefit_locit.py
@@ -1,6 +1,10 @@
 from typing import Union, List
 
-from astrohack.core.extract_locit import extract_spectral_info
+from astrohack.core.extract_locit import (
+    extract_spectral_info,
+    extract_antenna_data,
+    extract_source_and_telescope,
+)
 from astrohack.utils.file import overwrite_file
 from astrohack.utils.text import get_default_file_name
 from astrohack.io.position_mds import AstrohackPositionFile
@@ -8,8 +12,8 @@ from astrohack.io.position_mds import AstrohackPositionFile
 
 def fringefit_locit(
     fringefit_caltable: str,
-    position_name: str,
-    elevation_limit: float,
+    position_name: str | None = None,
+    elevation_limit: float | int = 10.0,
     polarization: str = "both",
     fit_engine: str = "scipy",
     fit_kterm: bool = False,
@@ -35,15 +39,16 @@ def fringefit_locit(
     )
 
     ddi_dict = extract_spectral_info(locit_params)
-
-    print(ddi_dict)
-
+    extract_antenna_data(locit_params, position_mds)
+    extract_source_and_telescope(locit_params, position_mds)
     position_mds.root.attrs.update(
         {
             "combined": True,
-            # "telescope_name": locit_mds.root.attrs["telescope_name"],
             # "reference_antenna": locit_mds.root.attrs["reference_antenna"],
         }
     )
+
+    position_mds.write("a")
+    position_mds.consolidate(["ant"])
 
     return

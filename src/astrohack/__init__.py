3b8cbacc16f87616ba2843efd8e0a7202d64cfd9
Victor de Souza Magalhaes <vdesouza@nrao.edu>
2026-07-22 08:55:35 -0600

    Fixed astrohack's __init__.py according to ruff's suggestions.

--- src/astrohack/__init__.py
+++ src/astrohack/__init__.py
@@ -3,44 +3,51 @@ from importlib.metadata import version
 
 __version__ = version("astrohack")
 
+from astrohack.io.dio import open_beamcut as open_beamcut
+from astrohack.io.dio import open_position as open_position
+from astrohack.io.dio import open_image as open_image
+from astrohack.io.dio import open_locit as open_locit
+from astrohack.io.dio import open_panel as open_panel
+from astrohack.io.dio import open_pointing as open_pointing
+from astrohack.io.dio import open_holog as open_holog
 
-from astrohack.io.dio import (
-    open_beamcut,
-    open_pointing,
-    open_holog,
-    open_image,
-    open_locit,
-    open_panel,
-    open_position,
-)
-from astrohack.io.point_mds import AstrohackPointFile
-from astrohack.io.locit_mds import AstrohackLocitFile
-from astrohack.io.holog_mds import AstrohackHologFile
-from astrohack.io.panel_mds import AstrohackPanelFile
-from astrohack.io.beamcut_mds import AstrohackBeamcutFile
-from astrohack.io.position_mds import AstrohackPositionFile
-from astrohack.io.image_mds import AstrohackImageFile
-from .extract_holog import (
-    extract_holog,
-    generate_holog_obs_dict,
-)
-from .beamcut import beamcut
-from .extract_pointing import extract_pointing
-from .holog import holog
-from .panel import panel
-from .combine import combine
-from .locit import locit
-from .extract_locit import extract_locit
-from .fringefit_locit import fringefit_locit
+from astrohack.io.point_mds import AstrohackPointFile as AstrohackPointFile
+from astrohack.io.locit_mds import AstrohackLocitFile as AstrohackLocitFile
+from astrohack.io.holog_mds import AstrohackHologFile as AstrohackHologFile
+from astrohack.io.panel_mds import AstrohackPanelFile as AstrohackPanelFile
+from astrohack.io.beamcut_mds import AstrohackBeamcutFile as AstrohackBeamcutFile
+from astrohack.io.position_mds import AstrohackPositionFile as AstrohackPositionFile
+from astrohack.io.image_mds import AstrohackImageFile as AstrohackImageFile
+
+from .extract_holog import extract_holog as extract_holog
+from .extract_holog import generate_holog_obs_dict as generate_holog_obs_dict
+from .beamcut import beamcut as beamcut
+from .extract_pointing import extract_pointing as extract_pointing
+from .holog import holog as holog
+from .panel import panel as panel
+from .combine import combine as combine
+from .locit import locit as locit
+from .extract_locit import extract_locit as extract_locit
+from .fringefit_locit import fringefit_locit as fringefit_locit
 from .cassegrain_ray_tracing import (
-    cassegrain_ray_tracing_pipeline,
-    create_ray_tracing_telescope_parameter_dict,
-    plot_2d_maps_from_rt_xds,
-    plot_radial_projection_from_rt_xds,
-    apply_holog_phase_fitting_to_rt_xds,
+    cassegrain_ray_tracing_pipeline as cassegrain_ray_tracing_pipeline,
 )
-from .image_comparison_tool import compare_fits_images, rms_table_from_zarr_datatree
-from .antenna.telescope import get_proper_telescope
+from .cassegrain_ray_tracing import (
+    create_ray_tracing_telescope_parameter_dict as create_ray_tracing_telescope_parameter_dict,
+)
+from .cassegrain_ray_tracing import plot_2d_maps_from_rt_xds as plot_2d_maps_from_rt_xds
+from .cassegrain_ray_tracing import (
+    plot_radial_projection_from_rt_xds as plot_radial_projection_from_rt_xds,
+)
+from .cassegrain_ray_tracing import (
+    apply_holog_phase_fitting_to_rt_xds as apply_holog_phase_fitting_to_rt_xds,
+)
+
+from .image_comparison_tool import compare_fits_images as compare_fits_images
+from .image_comparison_tool import (
+    rms_table_from_zarr_datatree as rms_table_from_zarr_datatree,
+)
+from .antenna.telescope import get_proper_telescope as get_proper_telescope
 
 # This installs a slick, informational tracebacks logger
 from rich.traceback import install
